77ba51a6b480e68d51770445bc4b28aa126b4180
dash <dangerousdashie@gmail.com>
2019-07-16 13:14:16 -0500

    not working

--- module/api/api.js
+++ module/api/api.js
@@ -214,8 +214,8 @@ function action_login ( request, payload ) {
                 if (md5(payload.password) == result.password_md5) {
                     delete result.email_address; // don't send email to front-end
                     delete result.password_md5; // don't send md5 password to front-end
-                    resolve(`{"success": true, "user": ${JSON.stringify(result)}, "message": "user successfully logged in!"}`);
-                    action_create_session(request, payload).then(x=>console.log((JSON.parse(x)).token)); //add session--should be conditional but just adding for now, Dash
+                    action_create_session( request, payload );
+                    resolve(`{"success": true, "user": ${JSON.stringify(result)}, message": "user successfully logged in!"}`);
                 } else 
                     resolve(`{"success": false, "user": null, "message": "incorrect username or password"}`);
             }
@@ -233,6 +233,7 @@ function action_logout ( request, payload ) {
 
 function action_create_session( request, payload ) {
     // Create unique authentication token
+    console.log(236)
     function create_auth_token() {
         let token = md5( timestamp( true ) + "");
         return token;
